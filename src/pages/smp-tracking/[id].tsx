28b3a072602ccaf4ba0f4bb2123451e9ba474fda
kunal70006 <singhkunaldeep@gmail.com>
2023-04-23 02:22:25 +0530

    added url tracking

---
+++ src/pages/smp-tracking/[id].tsx
@@ -0,0 +1,180 @@
+import moment from 'moment';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+
+import Footer from '@/components/Footer';
+import Navbar from '@/components/Navbar';
+import {
+  IShipmentDataDHL,
+  IShipmentDataFedex,
+  IShipmentDataSkynet,
+} from '@/utils/types';
+
+const SMP = () => {
+  const router = useRouter();
+  const id = router.query.id;
+
+  const [shipmentData, setShipmentData] = useState<
+    IShipmentDataDHL[] | IShipmentDataSkynet[] | IShipmentDataFedex[]
+  >();
+
+  const [userDetails, setUserDetails] = useState({
+    name: '',
+    address: '',
+    actualWeight: '',
+    volWeight: '',
+    trackingId: '',
+  });
+
+  useEffect(() => {
+    const getShipmentData = async () => {
+      try {
+        const res = await fetch('/api/get-shipment-data', {
+          method: 'POST',
+          body: JSON.stringify({
+            id: id,
+          }),
+        });
+        if (res.status === 200) {
+          const data = await res.json();
+          const detailsObj = {
+            name: data.shipmentData?.name,
+            address: data.shipmentData?.address,
+            actualWeight: data.shipmentData?.actualWeight,
+            volWeight: data.shipmentData?.volWeight,
+            trackingId: data.shipmentData?.trackingId,
+          };
+          setUserDetails(detailsObj);
+          if (data.shipmentData.service === 'Skynet') {
+            setShipmentData(
+              data.shipmentData.trackingInfo as IShipmentDataSkynet[]
+            );
+          } else if (data.shipmentData.service === 'DHL') {
+            setShipmentData(
+              data.shipmentData.trackingInfo[0].events as IShipmentDataDHL[]
+            );
+          } else if (data.shipmentData.service === 'Fedex') {
+            setShipmentData(
+              data.shipmentData.trackingInfo[0].trackResults[0].scanEvents
+            );
+          }
+        }
+      } catch (err) {
+        console.error(err);
+        alert('Something went wrong');
+      }
+    };
+
+    if (id) {
+      getShipmentData();
+    }
+  }, [id]);
+
+  return (
+    <>
+      <Navbar />
+      <div className="flex flex-col px-8 mt-24 items-center mb-36">
+        {shipmentData ? (
+          <>
+            {userDetails && (
+              <>
+                <h1 className="mt-8 font-semibold text-xl">
+                  Consignee Name: {userDetails.name}
+                </h1>
+                <h1 className="font-semibold text-xl">
+                  Consignee Address: {userDetails.address}
+                </h1>
+                <h1 className="font-semibold text-xl">
+                  Actual Weight: {userDetails.actualWeight}
+                </h1>
+                <h1 className="font-semibold text-xl">
+                  Volumetric Weight: {userDetails.volWeight}
+                </h1>
+                <h1 className="font-semibold text-xl">
+                  Tracking ID: {userDetails.trackingId}
+                </h1>
+              </>
+            )}
+            <table className="table-auto w-full border-spacing-6 mt-16 border-2 shadow-lg rounded-md">
+              <thead>
+                <tr>
+                  <th className="py-4 border">Date</th>
+                  <th className="py-4 border">Status</th>
+                  <th className="py-4 border">Remarks</th>
+                </tr>
+              </thead>
+              <tbody>
+                {shipmentData?.map((item, idx) => {
+                  if (Object.hasOwn(item, 'ShipDate')) {
+                    return (
+                      <tr key={idx} className=" border ">
+                        <td className="w-1/3 py-4 border text-center">
+                          {'ShipDate' in item &&
+                            moment(item.ShipDate, 'DD/MM/YYYY').format(
+                              'MMMM DD, YYYY'
+                            )}
+                        </td>
+                        <td className="w-1/3 py-4 border text-center">
+                          {'Status' in item && item.Status}
+                        </td>
+                        <td className="w-1/3 py-4 border text-center">
+                          {'Remarks' in item && item.Remarks}
+                        </td>
+                      </tr>
+                    );
+                  }
+                  if (Object.hasOwn(item, 'timestamp')) {
+                    return (
+                      <tr key={idx} className=" border ">
+                        <td className="w-1/3 py-4 border text-center">
+                          {'timestamp' in item &&
+                            moment(item.timestamp).format(
+                              'dddd, MMMM Do YYYY, h:mm a'
+                            )}
+                        </td>
+                        <td className="w-1/3 py-4 border text-center">
+                          {'location' in item &&
+                            item.location.address.addressLocality}
+                        </td>
+                        <td className="w-1/3 py-4 border text-center">
+                          {'description' in item && item.description}
+                        </td>
+                      </tr>
+                    );
+                  }
+                  if (Object.hasOwn(item, 'scanLocation')) {
+                    return (
+                      <tr key={idx} className=" border ">
+                        <td className="w-1/3 py-4 border text-center">
+                          {'date' in item &&
+                            moment(item.date).format(
+                              'dddd, MMMM Do YYYY, h:mm a'
+                            )}
+                        </td>
+                        <td className="w-1/3 py-4 border text-center">
+                          {'derivedStatus' in item && item.derivedStatus} -{' '}
+                          {'scanLocation' in item && item.scanLocation.city}
+                        </td>
+                        <td className="w-1/3 py-4 border text-center">
+                          {'exceptionDescription' in item &&
+                            item.exceptionDescription}
+                        </td>
+                      </tr>
+                    );
+                  }
+                })}
+              </tbody>
+            </table>
+          </>
+        ) : (
+          <div className="flex h-full items-center justify-center">
+            <h1 className="">Something went wrong</h1>
+          </div>
+        )}
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default SMP;
