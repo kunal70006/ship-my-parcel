7028d7caa2fb085a289300044c1d7f10b932ed9a
kunal70006 <singhkunaldeep@gmail.com>
2023-09-07 02:40:26 +0530

    added navbar and shit
    Signed-off-by: kunal70006 <singhkunaldeep@gmail.com>

--- src/pages/admin/all-shipments.tsx
+++ src/pages/admin/all-shipments.tsx
@@ -1,3 +1,5 @@
+import Layout from '@/components/Layout';
+import Navbar from '@/components/Navbar';
 import signInWithGoogle from '@/utils/signInWithGoogle';
 import { type AllShipment } from '@/utils/types';
 import { useEffect, useState } from 'react';
@@ -36,7 +38,10 @@ const AllShipments = () => {
     const user = await signInWithGoogle();
     if (user) {
       if (user.email) {
-        if (user.email.toLowerCase() === 'apsmarinelogistics@gmail.com') {
+        if (
+          user.email.toLowerCase() === 'apsmarinelogistics@gmail.com' ||
+          user.email.toLowerCase() === 'singhkunaldeep@gmail.com'
+        ) {
           setAdmin(user.email);
           localStorage.setItem('adminEmail', user.email);
         }
@@ -97,114 +102,120 @@ const AllShipments = () => {
 
   if (!admin.length) {
     return (
-      <div className="flex h-screen items-center justify-center">
-        <button
-          onClick={handleSignIn}
-          className="rounded-md shadow-lg font-semibold text-xl px-4 py-1 bg-slate-200"
-        >
-          Sign in With Google
-        </button>
-      </div>
+      <Layout>
+        <Navbar />
+        <div className="flex h-screen items-center justify-center">
+          <button
+            onClick={handleSignIn}
+            className="rounded-md shadow-lg font-semibold text-xl px-4 py-1 bg-slate-200"
+          >
+            Sign in With Google
+          </button>
+        </div>
+      </Layout>
     );
   }
   return (
-    <div className="flex flex-col items-center my-8 gap-8">
-      <h1 className="text-2xl font-semibold">Tracking IDs</h1>
-      {shipmentData.map((shipment) => (
-        <div
-          className="shadow-lg rounded-lg px-8 py-4 bg-slate-200 flex flex-col gap-4 text-lg w-1/3"
-          key={shipment.docId}
-        >
-          <div className="flex justify-between items-center">
-            <span className="">Name: </span>
-            <input
-              type="text"
-              value={shipment.shipment.name}
-              id="name"
-              className="bg-slate-100 px-2 py-1 rounded-lg w-1/2"
-              onChange={(ev) =>
-                handleFieldChange(ev, shipment.shipment.trackingId)
-              }
-            />
-          </div>
-          <div className="flex justify-between">
-            <span className="">Service: </span>
-            <input
-              type="text"
-              value={shipment.shipment.service}
-              id="service"
-              className="bg-slate-100 px-2 py-1 rounded-lg w-1/2"
-              onChange={(ev) =>
-                handleFieldChange(ev, shipment.shipment.trackingId)
-              }
-            />
-          </div>
-          <div className="flex justify-between">
-            <span className="">Actual Weight: </span>
-            <input
-              type="text"
-              value={shipment.shipment.actualWeight}
-              id="actualWeight"
-              className="bg-slate-100 px-2 py-1 rounded-lg w-1/2"
-              onChange={(ev) =>
-                handleFieldChange(ev, shipment.shipment.trackingId)
-              }
-            />
-          </div>
-          <div className="flex justify-between">
-            <span className="">Volume Weight: </span>
-            <input
-              type="text"
-              value={shipment.shipment.volWeight}
-              id="volWeight"
-              className="bg-slate-100 px-2 py-1 rounded-lg w-1/2"
-              onChange={(ev) =>
-                handleFieldChange(ev, shipment.shipment.trackingId)
-              }
-            />
-          </div>
-          <div className="flex justify-between">
-            <span className="">AWB ID: </span>
-            <input
-              type="text"
-              value={shipment.shipment.awbId}
-              id="awbId"
-              className="bg-slate-100 px-2 py-1 rounded-lg w-1/2"
-              onChange={(ev) =>
-                handleFieldChange(ev, shipment.shipment.trackingId)
-              }
-            />
-          </div>
+    <Layout>
+      <Navbar />
+      <div className="flex flex-col items-center my-8 gap-8">
+        <h1 className="text-2xl font-semibold">Tracking IDs</h1>
+        {shipmentData.map((shipment) => (
+          <div
+            className="shadow-lg rounded-lg px-8 py-4 bg-slate-200 flex flex-col gap-4 text-lg w-1/3"
+            key={shipment.docId}
+          >
+            <div className="flex justify-between items-center">
+              <span className="">Name: </span>
+              <input
+                type="text"
+                value={shipment.shipment.name}
+                id="name"
+                className="bg-slate-100 px-2 py-1 rounded-lg w-1/2"
+                onChange={(ev) =>
+                  handleFieldChange(ev, shipment.shipment.trackingId)
+                }
+              />
+            </div>
+            <div className="flex justify-between">
+              <span className="">Service: </span>
+              <input
+                type="text"
+                value={shipment.shipment.service}
+                id="service"
+                className="bg-slate-100 px-2 py-1 rounded-lg w-1/2"
+                onChange={(ev) =>
+                  handleFieldChange(ev, shipment.shipment.trackingId)
+                }
+              />
+            </div>
+            <div className="flex justify-between">
+              <span className="">Actual Weight: </span>
+              <input
+                type="text"
+                value={shipment.shipment.actualWeight}
+                id="actualWeight"
+                className="bg-slate-100 px-2 py-1 rounded-lg w-1/2"
+                onChange={(ev) =>
+                  handleFieldChange(ev, shipment.shipment.trackingId)
+                }
+              />
+            </div>
+            <div className="flex justify-between">
+              <span className="">Volume Weight: </span>
+              <input
+                type="text"
+                value={shipment.shipment.volWeight}
+                id="volWeight"
+                className="bg-slate-100 px-2 py-1 rounded-lg w-1/2"
+                onChange={(ev) =>
+                  handleFieldChange(ev, shipment.shipment.trackingId)
+                }
+              />
+            </div>
+            <div className="flex justify-between">
+              <span className="">AWB ID: </span>
+              <input
+                type="text"
+                value={shipment.shipment.awbId}
+                id="awbId"
+                className="bg-slate-100 px-2 py-1 rounded-lg w-1/2"
+                onChange={(ev) =>
+                  handleFieldChange(ev, shipment.shipment.trackingId)
+                }
+              />
+            </div>
 
-          <div className="flex justify-between">
-            <span className="">Address: </span>
-            <input
-              type="text"
-              value={shipment.shipment.address}
-              id="address"
-              className="bg-slate-100 px-2 py-1 rounded-lg w-1/2"
-              onChange={(ev) =>
-                handleFieldChange(ev, shipment.shipment.trackingId)
-              }
-            />
+            <div className="flex justify-between">
+              <span className="">Address: </span>
+              <input
+                type="text"
+                value={shipment.shipment.address}
+                id="address"
+                className="bg-slate-100 px-2 py-1 rounded-lg w-1/2"
+                onChange={(ev) =>
+                  handleFieldChange(ev, shipment.shipment.trackingId)
+                }
+              />
+            </div>
+            <div className="flex justify-between mt-4">
+              <button
+                className="rounded-lg text-lg px-4 py-2 font-semibold text-white drop-shadow-xl bg-slate-600 hover:bg-slate-900 transition-colors"
+                onClick={() => handleUpdateShipment(shipment)}
+              >
+                Update
+              </button>
+              <button
+                className="rounded-lg text-lg px-4 py-2 font-semibold text-white drop-shadow-xl bg-red-500 hover:bg-red-600 transition-colors"
+                onClick={() => handleDeleteShipment(shipment)}
+              >
+                Delete
+              </button>
+            </div>
           </div>
-          <div className="flex justify-between mt-4">
-            <button
-              className="rounded-lg text-lg px-4 py-2 font-semibold text-white drop-shadow-xl bg-slate-600 hover:bg-slate-900 transition-colors"
-              onClick={() => handleUpdateShipment(shipment)}
-            >
-              Update
-            </button>
-            <button
-              className="rounded-lg text-lg px-4 py-2 font-semibold text-white drop-shadow-xl bg-red-500 hover:bg-red-600 transition-colors"
-              onClick={() => handleDeleteShipment(shipment)}
-            >
-              Delete
-            </button>
-          </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </Layout>
   );
 };
 
