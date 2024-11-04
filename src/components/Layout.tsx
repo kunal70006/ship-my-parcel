33bbea3bfb72e466cc221bf080e099d262dc5285
Kunaldeep Singh <kunaldeep@pushowl.com>
2024-11-04 22:23:00 +0530

    updated layout logo

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -17,7 +17,7 @@ const Layout = ({ children }: any) => {
         />
         <meta
           property="og:image"
-          content="https://firebasestorage.googleapis.com/v0/b/ship-my-parcel-41b0f.appspot.com/o/logo.png?alt=media&token=b93ecfd9-2c85-4248-9496-3239bb504943"
+          content="https://firebasestorage.googleapis.com/v0/b/ship-my-parcel-demo.appspot.com/o/logo.JPG?alt=media&token=b4348f71-d67e-4342-8814-c74fc2223933/o/logo.png?alt=media&token=b93ecfd9-2c85-4248-9496-3239bb504943"
         />
       </Head>
       {children}
